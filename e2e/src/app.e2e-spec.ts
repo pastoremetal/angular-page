227ccfba53fe6f34f39c9de7b0f1b700b1b1bf2f
pastoremetal <pastoremetal@yahoo.com.br>
2019-08-07 22:18:54 -0300

    project change

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-serverless!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-page!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-serverless'),
+      dir: require('path').join(__dirname, './coverage/angular-page'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
